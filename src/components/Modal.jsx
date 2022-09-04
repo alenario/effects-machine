4e08e921a9a7cfa34016e239e9520704e3c44d93
Ale Nario <alenario7@gmail.com>
2022-09-04 19:38:39 +0200

    starting to work on modal content

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import imageMachine from './0-the-effects-machine.png'
 
 export const Modal = ({ visible, onClose }) => {
-  const handleOnClose = (e) => {
-    if (e.target.id === 'modalContainer') onClose();
-  }  
-  
-  if (!visible) return null
+    const handleOnClose = (e) => {
+        if (e.target.id === 'modalContainer') onClose()
+    }
+
+    if (!visible) return null
 
     return (
-        // backdrop-blur-sm, justify-center:Horizontal, items-center:Vertical
-        <div id='modalContainer' onClick={handleOnClose} className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-[2px] flex justify-center items-center">
-            <div className="bg-white p-2 rounded">
-                <p>My modal</p>
-                <p>Hello!</p>
-                <button onClick={onClose}>X</button>
+
+        <div
+            id="modalContainer"
+            onClick={handleOnClose}
+            className="fixed inset-0 bg-black bg-opacity-40 backdrop-blur-[2px] flex justify-center items-center"
+        >
+            {/* Flex starts */}
+            <div className="bg-white p-2 rounded-md flex flex-row justify-center h-[50vh]">
+                {/* Column 1-L: Image  */}
+                <div>
+                    <img src={imageMachine} className="h-[80vh]" alt="/" />
+                </div>
+
+                {/* Column 2-R: Text  */}
+                <div>
+                    <p>My modal</p>
+                    <p>Hello!</p>
+                    <button onClick={onClose}>X</button>
+                </div>
             </div>
         </div>
     )
